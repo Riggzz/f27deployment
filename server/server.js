6f9095c65ed7d8c037da078fecfbf5ff0d8da195
= <=>
2022-08-31 12:39:05 -0600

    update

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 
 //endpoints
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index/html"))
 })
 
