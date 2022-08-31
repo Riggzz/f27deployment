a21262daf781957cbd7300cc4f362ec72acfcade
= <=>
2022-08-31 12:50:48 -0600

    update 3

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const app = express()
 //endpoints
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
-})
+});
+
+app.get('/css', () => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"))
+});
 
 
 const port = process.env.PORT || 4005;
